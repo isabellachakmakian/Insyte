9b1d43b35cd8cb46558450e7cce07ffc0d2328ef
Frankye Perez <fperezc3@calstatela.edu>
2026-05-01 15:12:36 -0700

    feat: add app list component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,19 @@
 import './App.css'; // Import App styling 
 import SearchPage from './pages/search/SearchPage.jsx';
 import ReportPage from './pages/report/ReportPage.jsx';
+import ListPage from './pages/list/ListPage.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import React bootstrap
 
 // react-router-dom imports
-import {RouterProvider, createBrowserRouter, createRoutesFromElements, Route} from 'react-router-dom';
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 
 // Create router with JSX Route elements 
 const appRouter = createBrowserRouter(createRoutesFromElements(
-<Route path="/">
-  <Route index element={<SearchPage />} />
-  <Route path= "report/:appId/:name" element={<ReportPage />} />
-</Route>));
+  <Route path="/">
+    <Route index element={<SearchPage />} />
+    <Route path="report/:appId/:name" element={<ReportPage />} />
+    <Route path="list" element={<ListPage />} />
+  </Route>));
 
 function App() {
 
